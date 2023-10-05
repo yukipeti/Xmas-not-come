552ccb5b7369f6cff09027eacce03a3a16fb2511
yukipeti <ugachan0817@gmail.com>
2023-10-06 02:40:08 +0900

    なんかいろいろしたよえらいね

---
+++ index.js
@@ -0,0 +1,50 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { LISTENER } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+// commandsフォルダから、.jsで終わるファイルのみを取得
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // 取得した.jsファイル内の情報から、コマンドと名前をListenner-botに対して設定
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING]  ${filePath} のコマンドには、必要な "data" または "execute" プロパティがありません。`);
+    }
+}
+
+// コマンドが送られてきた際の処理
+client.on(Events.InteractionCreate, async interaction => {
+    // コマンドでなかった場合は処理せずさよなら。
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    // 一致するコマンドがなかった場合
+    if (!command) {
+        console.error(` ${interaction.commandName} というコマンドは存在しません。`);
+        return;
+    }
+
+    try {
+        // コマンドを実行
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'コマンドを実行中にエラーが発生しました。', ephemeral: true });
+    }
+});
+
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(LISTENER.TOKEN);
