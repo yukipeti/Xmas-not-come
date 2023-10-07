86a04f86a919a81accc48277ae0478ca75265ed4
yukipeti <ugachan0817@gmail.com>
2023-10-08 02:47:51 +0900

    apex終わった後にvaloするとマジで当たる

--- commands/set-message.js
+++ commands/set-message.js
@@ -5,17 +5,17 @@ module.exports = [
     data: new SlashCommandBuilder()
       .setName("set-message")
       .setDescription("メッセージに関する設定をします")
+      .setDefaultMemberPermissions(1 << 3)
+      .setDMPermission(false)
       .addBooleanOption((option) =>
         option
           .setName("come-message")
           .setDescription("メッセージを送信するか設定できます(default: ture)")
-          /*.setDefaultMemberPermissions(1<<3)
-                    .setDMPermission(false)*/
           .setRequired(true),
       ),
 
     async execute(interaction) {
-      //await interaction.deferReply({ephemeral: true});
+      await interaction.deferReply({ ephemeral: true });
       await interaction.reply("nunti");
     },
   },
